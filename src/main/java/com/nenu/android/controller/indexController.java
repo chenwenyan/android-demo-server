02a4d6819fa33c13645a966e1c634fb888f6a98c
chenwenyan <1358384755@qq.com>
2017-05-09 13:14:31 +0800

    do a request test

---
+++ src/main/java/com/nenu/android/controller/indexController.java
@@ -0,0 +1,48 @@
+package com.nenu.android.controller;
+
+import com.nenu.android.service.IndexService;
+import net.sf.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * indexController
+ *
+ * @author: wychen
+ * @time: 2017/5/8 14:05
+ */
+@Controller
+public class indexController {
+
+    @Autowired
+    private IndexService indexService;
+
+    @RequestMapping(value = "/test",method = RequestMethod.POST)
+    @ResponseBody
+    public JSONObject test(HttpServletRequest request, HttpServletResponse response,
+                           Model model){
+        JSONObject jsonObject = new JSONObject();
+        String test = request.getParameter("test");
+        System.out.println(test);
+        try{
+            String result = indexService.test(test);
+            jsonObject.put("code",1);
+            jsonObject.put("msg","success");
+            jsonObject.put("data",result);
+
+        }catch (Exception e){
+            jsonObject.put("code",0);
+            jsonObject.put("msg","failed");
+            e.printStackTrace();
+        }
+        return jsonObject;
+
+    }
+}
---
+++ src/main/java/com/nenu/android/service/IndexService.java
@@ -0,0 +1,13 @@
+package com.nenu.android.service;
+
+/**
+ * IndexService
+ *
+ * @author: wychen
+ * @time: 2017/5/8 14:10
+ */
+public interface IndexService {
+
+    public String test(String test)throws Exception;
+
+}
